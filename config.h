fd5df48a93295bcf8ac11b4f8d1e65625302a68c
prosp3ro <g0lden_vo1d@protonmail.com>
2023-10-28 10:43:05 +0200

    gruvbox

--- config.h
+++ config.h
@@ -96,7 +96,7 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	[0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	[0] = "#000000", /* hard contrast: #1d2021 / soft contrast: #32302f */
 	[1] = "#cc241d", /* red     */
 	[2] = "#98971a", /* green   */
 	[3] = "#d79921", /* yellow  */
