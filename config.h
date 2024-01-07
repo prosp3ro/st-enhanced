c40c1fb61928cb5f9c665b227165af937e430661
prosp3ro <g0lden_vo1d@protonmail.com>
2024-01-07 21:37:07 +0100

    sync

--- config.h
+++ config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true";
+// static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
 // static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
