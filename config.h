6b0cdf77c3ab6da5106f4b2cec2347fcc14079fb
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2024-09-12 19:24:24 +0200

    sync

--- config.h
+++ config.h
@@ -5,14 +5,14 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=13:antialias=true:autohint=true";
-// static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
-// static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=11:antialias=true:autohint=true";
+// static char *font = "Liberation Mono:pixelsize=11:antialias=true:autohint=true";
+// static char *font = "mono:pixelsize=11:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
-    // "Inconsolata for Powerline:pixelsize=14:antialias=true:autohint=true",
-    "Hack Nerd Font Mono:pixelsize=13:antialias=true:autohint=true",
-    "NotoColorEmoji:pixelsize=13:antialias=true:autohint=true",
+    // "Inconsolata for Powerline:pixelsize=11:antialias=true:autohint=true",
+    "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
+    "NotoColorEmoji:pixelsize=11:antialias=true:autohint=true",
 };
 
 static int borderpx = 1;
