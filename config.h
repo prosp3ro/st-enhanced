fc863ebfcbb1b36d87607d6e033e9fdf29eacd09
prosp3ro <70239480+prosp3ro@users.noreply.github.com>
2024-11-30 16:38:42 +0100

    sync

--- config.h
+++ config.h
@@ -104,7 +104,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.89;
+float alpha = 0.91;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
